34d5f6a4aa0c8b0628cdc1f671d1b15e0f7082d0
Jithin Puthiyattu <j.puthiyattu@walmart.com>
2023-04-26 22:04:22 +0530

    [INIT] Inital commit

---
+++ src/main/java/com/dreamblitz/webflux17/Webflux17Application.java
@@ -0,0 +1,13 @@
+package com.dreamblitz.webflux17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webflux17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamblitz/webflux17/Webflux17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamblitz.webflux17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webflux17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
