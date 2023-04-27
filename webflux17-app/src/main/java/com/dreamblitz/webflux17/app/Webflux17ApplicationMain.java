8c5d77bf9457306ef3b1204b03690ef9a7f4a377
Jithin Puthiyattu <j.puthiyattu@walmart.com>
2023-04-27 09:59:42 +0530

    [APP-MODULE] Added app module

--- webflux17-app/src/main/java/com/dreamblitz/webflux17/app/Webflux17ApplicationMain.java
+++ webflux17-app/src/main/java/com/dreamblitz/webflux17/app/Webflux17ApplicationMain.java
@@ -1,13 +1,13 @@
-package com.dreamblitz.webflux17;
+package com.dreamblitz.webflux17.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Webflux17Application {
+public class Webflux17ApplicationMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Webflux17Application.class, args);
+		SpringApplication.run(Webflux17ApplicationMain.class, args);
 	}
 
 }
